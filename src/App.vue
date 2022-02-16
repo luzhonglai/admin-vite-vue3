aa8899dc202b1ebbe9652b82edb045895e885f0e
ZhongLaiLu <32395032@qq.com>
2022-02-17 00:26:42 +0800

    ✨ feat: 添加pinia demo test

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { userStore } from "@/pinia/user";
+
+const mainStore = userStore();
+console.log("🚀 ~ file: App.vue ~ line 8 ~ mainStore", mainStore.count)
+
+//  patch 直接修改state
+mainStore.$patch({
+  count:'222'
+})
+
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite"></HelloWorld>
   <!-- 测试taiwindcss 插件 -->
-  <div  class="flex w-36 h-36  m-auto ">欢迎学习vue3 script setup pinia </div>
-  <div class="card text-white bg-primary">
-    <img class="card-img-top" src="holder.js/100px180/" alt="">
-    <div class="card-body">
-      <h4 class="card-title">Title</h4>
-      <p class="card-text">Text</p>
-    </div>
-  </div>
+  <div class="flex w-36 h-36 m-auto items-center">欢迎学习vue3 script setup pinia</div>
+  <div @click="mainStore.setCount">{{mainStore.count}}</div>
 </template>
 
 <style>
---
+++ src/pinia/user.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+
+
+export const  userStore =  defineStore({
+  id: '111',
+  state:()=>({
+    count: 'rare'
+  }),
+  actions:{ 
+    setCount(){
+      this.count += 'add code  date'
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
